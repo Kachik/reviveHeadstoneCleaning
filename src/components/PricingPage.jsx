f484a9a4079ca4eebd4904d46eb36b5b57627a8d
Khachik Ashkaryan <khachikashkaryan@gmail.com>
2023-07-06 05:14:01 +0000

    Added Stripe for payment.

---
+++ src/components/PricingPage.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+function PricingPage() {
+  // Paste the stripe-pricing-table snippet in your React component
+  return (
+    <stripe-pricing-table
+      pricing-table-id="prctbl_1NQjzgB2xYH5zM7CNuHKeYp6"
+      publishable-key="pk_test_51NQiwgB2xYH5zM7CKIn54JBE51mvFdFM2cdyNhrM5xc4RhQdLszj2Da6LLDBFWUX17bdlWkVAW3eCAf9hTEN49WJ008fafAyjv"
+    ></stripe-pricing-table>
+  );
+}
+
+export default PricingPage;
