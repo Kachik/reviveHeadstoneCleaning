6ed9ac925702a6a44aca0118a61ad93ca86c1d47
Khachik Ashkaryan <44388176+Kachik@users.noreply.github.com>
2024-02-28 06:30:24 +0000

    Adjust for Aram's pricing preferences.

--- src/components/ServicesTotal.js
+++ src/components/ServicesTotal.js
@@ -23,29 +23,29 @@ function Trip() {
         />
         <TripData
           image={Trip2}
-          heading="Monthly Cleaning &#183; $49.99"
+          heading="Monthly Cleaning &#183; $59.99"
           text="After the first clean, we are available to maintain the headstone with cleanings every month. For additional flowers, grass cutting, incense burning, and flower pot cleaning: +$39.99."
         />
         <TripData
           image={Trip3}
-          heading="Monthly Flower Arrangement &#183; $59.99"
+          heading="Monthly Flower Arrangement &#183; $74.99"
           text="If cleaning is not desired, we also offer monthly delivery of flowers to headstones. This includes any kind of flower and bouquet that we source from a local flower shop."
         />
       </div>
       <div className="tripcard">
         <TripData
           image={Cleaning}
-          heading="Crack and Stone Repair &#183; $239.99"
+          heading="Crack and Stone Repair &#183; Contact for Pricing"
           text="After some time, the headstone is prone to cracks and color fading. With safe and lasting chemicals and stone, we repair the headstone and clean to look as good as new."
         />
         <TripData
           image={Painting}
-          heading="Letter Coloring &#183; $49.99"
+          heading="Letter Coloring &#183; Contact for Pricing"
           text="The color on headstone lettering fades no matter the material used. When cleaning is not able to rejuvinate the headstone, it is important to repaint the lettering before cleaning so the headstone looks good as new."
         />
         <TripData
           image={TotalService}
-          heading="Full Subscription Service &#183; $109.99 + $49.99 Monthly"
+          heading="Full Subscription Service &#183; $109.99 + $59.99 Monthly"
           text="For the deep renovation and monthly cleanings, we offer a $80 discount on the first clean with the first year of monthly cleanings paid."
         />
       </div>
